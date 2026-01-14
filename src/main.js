821c312fd6fe161a40521974ed06f9e4d15fb07e
etnevelpriv <leventerastovits@gmail.com>
2026-01-14 11:38:46 +0100

    Urlap torles reset funkcio kesz.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 import './style.css'
+
+const sendForm = function (e) {
+  document.getElementById("formContainer").reset();
+  console.log('reset kesz');
+  e.preventDefault();
+};
+
+document.getElementById('sendFormButton').addEventListener('click', sendForm);
