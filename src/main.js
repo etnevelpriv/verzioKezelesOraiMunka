0637349c1b5799f2335c64bbcf1509d95eeb9eac
etnevelpriv <leventerastovits@gmail.com>
2026-01-14 11:46:55 +0100

    Sikeres regisztracio felirat keszen van

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './style.css'
 
 const sendForm = function (e) {
   document.getElementById("formContainer").reset();
+  document.getElementById("successText").textContent = 'Sikeres regisztráció';
   console.log('reset kesz');
   e.preventDefault();
 };
